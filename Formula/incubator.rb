ad0d42525ffd875f05dad68213e95def8e140283
terra tauri <me@terratauri.com>
2026-03-14 21:36:22 -0700

    Add incubator formula v0.2.1

---
+++ Formula/incubator.rb
@@ -0,0 +1,30 @@
+class Incubator < Formula
+  desc "A team of Claude agents that takes an idea from research to release"
+  homepage "https://github.com/terraboops/incubator"
+  url "https://github.com/terraboops/incubator/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "d7502535e2325bfac4a2c1e6096effdd59a2cbca2188f124310ab4192ab92de0"
+  license "MIT"
+
+  depends_on "python@3.12"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  def caveats
+    <<~EOS
+      To get started:
+        incubator init myproject
+        cd myproject
+        incubator serve
+
+      Configure your .env file with Telegram credentials for
+      human-in-the-loop approval gates. See:
+        https://github.com/terraboops/incubator#configuration
+    EOS
+  end
+
+  test do
+    assert_match "incubator", shell_output("#{bin}/incubator --help")
+  end
+end
