759bf2cfbedcf49ac1662a7eeb15de1bed2788b2
terra tauri <me@terratauri.com>
2026-03-16 18:46:36 -0700

    incubator 0.3.0

--- Formula/incubator.rb
+++ Formula/incubator.rb
@@ -3,8 +3,8 @@ class Incubator < Formula
 
   desc "A team of Claude agents that takes an idea from research to release"
   homepage "https://github.com/terraboops/incubator"
-  url "https://github.com/terraboops/incubator/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "d7502535e2325bfac4a2c1e6096effdd59a2cbca2188f124310ab4192ab92de0"
+  url "https://github.com/terraboops/incubator/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "bc416bbe036b4d5c078d0aca1559ddc8f494ab6ec38ccf0b5dbb36d9d41dfd66"
   license "MIT"
 
   depends_on "python@3.12"
