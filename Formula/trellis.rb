5905fa42b653bff88b8667a7493cf98751043150
terra tauri <me@terratauri.com>
2026-04-05 17:11:23 -0700

    trellis 1.1.4

--- Formula/trellis.rb
+++ Formula/trellis.rb
@@ -3,8 +3,8 @@ class Trellis < Formula
 
   desc "Agentic pipeline platform - design agent teams that take ideas from concept to launch"
   homepage "https://github.com/terraboops/trellis"
-  url "https://github.com/terraboops/trellis/archive/refs/tags/v1.1.3.tar.gz"
-  sha256 "2964bf369c45880790cb5ffad354e27c2c941db89981ce29e53df7669357fdf4"
+  url "https://github.com/terraboops/trellis/archive/refs/tags/v1.1.4.tar.gz"
+  sha256 "7047d76d87389ef48055df34d6ec5059c8dcb5a029223075e8ae3119c3775590"
   license "Apache-2.0"
 
   depends_on "python@3.12"
