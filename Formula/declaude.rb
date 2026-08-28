2561c5c4823ee092c95ffa969616effc33e2e597
terraboops <terra@terraboops>
2026-08-27 17:58:43 -0700

    style: drop redundant version (inferred from URL)

--- Formula/declaude.rb
+++ Formula/declaude.rb
@@ -3,7 +3,6 @@ class Declaude < Formula
   homepage "https://github.com/terraboops/declaude"
   url "https://github.com/terraboops/declaude/releases/download/v0.1.0/declaude-macos-arm64"
   sha256 "d8f4d19b1f223dcf48e4b35f1aa99f4f579ee267d2a036b5484964317e5f46a0"
-  version "0.1.0"
   license "MIT"
 
   depends_on arch: :arm64 # MLX requires Apple Silicon
